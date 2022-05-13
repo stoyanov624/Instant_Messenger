1cf779f427b66c5686de2d300486eb671422c704
Ivo <paradoxa624@gmail.com>
2022-05-14 00:00:52 +0300

    returned server folder after adding gitignore for node modules

---
+++ server/src/app.ts
@@ -0,0 +1,9 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+
+const app : Application = express();
+
+app.get('/', (req : Request, res : Response) => {
+    res.send('Hello world!');
+})
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server listening at port: ${PORT}`))
