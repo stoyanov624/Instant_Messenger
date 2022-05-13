1f15cc707f670a59e0b56d97dcdf281ebc2ba182
Ivo <paradoxa624@gmail.com>
2022-05-13 23:55:45 +0300

    removed server folder

--- server/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
-
-const app : Application = express();
-
-app.get('/', (req : Request, res : Response) => {
-    res.send('Hello world!');
-})
-const PORT = 8080;
-app.listen(PORT, () => console.log(`Server listening at port: ${PORT}`))
