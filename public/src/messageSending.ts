75864e42c3c37a9e1e9847e44d3ea2b938e6bf42
Ivo <paradoxa624@gmail.com>
2022-05-27 10:51:10 +0300

    added socket io to server side

--- public/src/messageSending.ts
+++ public/src/messageSending.ts
@@ -1,3 +1,6 @@
+import {io} from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 const input = document.getElementById('msg-sender') as HTMLInputElement | null;
 const messages = document.getElementById('chat');
 
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,22 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-const app : Application = express();
+const app : Application = express(); 
+app.use(express.json());
+
+const server = createServer(app);
+const io = new Server(server, {cors: {origin: ['http://localhost:8080']}});
+
+const PORT = 3000;
 
 app.get('/', (req : Request, res : Response) => {
     res.send('Hello world!');
 })
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server listening at port: ${PORT}`))
+
+io.on('connection', socket => {
+    console.log('Hi');
+    console.log(socket.id);
+});
+
+server.listen(PORT, () => console.log(`Server listening at port: ${PORT}`))
