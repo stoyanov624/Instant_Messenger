2445170d53b2529c06d76447002dd910c4055d74
Ivo <paradoxa624@gmail.com>
2022-06-10 00:36:06 +0300

    using dotenv for database connection

--- server/src/databates-connector.ts
+++ server/src/databates-connector.ts
@@ -2,12 +2,12 @@ import {DataSource} from "typeorm";
 import { User } from "./entities/User";
 
 const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "Ezikiel22",
-  database: "Instant_Messenger",
+  type: 'postgres',
+  host: process.env.ORM_HOST,
+  port: Number(process.env.ORM_PORT),
+  username: process.env.ORM_USERNAME,
+  password: process.env.ORM_PASSWORD,
+  database: process.env.ORM_DATABASE,
   entities: [User],
   synchronize: false,
   logging: false,
