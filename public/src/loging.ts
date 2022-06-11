71bb90962c58f6d09135adf9a3383d832df339a7
Ivo <paradoxa624@gmail.com>
2022-06-11 16:32:57 +0300

    made forms swap from display none to default display on click

--- public/src/loging.ts
+++ public/src/loging.ts
@@ -1,4 +1,9 @@
 const startButton = document.getElementById('start');
+const registerButton = document.getElementById('reg-btn');
+const backButton = document.getElementById('back-btn');
+const loginForm = document.getElementById('login');
+const registerForm = document.getElementById('register');
+
 const usernameField = document.getElementById('username-field') as HTMLInputElement;
 startButton?.addEventListener('click', () => {
     if(usernameField?.value.trim()) {
@@ -6,5 +11,19 @@ startButton?.addEventListener('click', () => {
         window.location.replace('http://localhost:8080/home.html')
     }
 }); 
+
+backButton?.addEventListener('click', () => {
+    if (registerForm && loginForm) {
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'block'
+    }
+})
+
+registerButton?.addEventListener('click', () => {
+    if (registerForm && loginForm) {
+        registerForm.style.display = 'block';
+        loginForm.style.display = 'none'
+    }
+})
 
 export {};
