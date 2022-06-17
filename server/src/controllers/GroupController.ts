828292b219171a6b50ad228caf9070eddd3a5457
Ivo <paradoxa624@gmail.com>
2022-06-17 16:04:15 +0300

    added saving and fetching of group messages

--- server/src/controllers/GroupController.ts
+++ server/src/controllers/GroupController.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import { AppDataSource } from '../databates-connector';
 import { ChatGroup } from '../entities/ChatGroup.entity';
 import { User } from '../entities/User.entity';
+import { Message } from '../entities/Message.entity';
 
 export class GroupController {
     router: express.Router;
@@ -13,9 +14,8 @@ export class GroupController {
     }
 
     private createRoutes() {
-        this.router.get('/messages/:groupId');
-        this.router.post('/messages/:groupId');
-        this.router.post('/:userId');
+        this.router.get('/messages/:groupId', this.getGroupMessages);
+        this.router.post('/messages/:groupId', this.saveMessageToGroup);
         this.router.post('/join', this.assignUser)
     }
 
@@ -57,5 +57,48 @@ export class GroupController {
 
     private async generateGroups(request: Express.Request, response: Express.Response){
         
+    }
+
+    private async getGroupMessages(response: express.Response, request: express.Request) {
+        try {
+            const groupId = Number(request.params.groupId);
+            const group : ChatGroup = await AppDataSource.manager.findOne(ChatGroup, {
+                relations: ['messages'],
+                where: {id: groupId}
+            }); 
+
+            if(!group) {
+                throw new Error("Group not found!");
+            }
+
+            response.status(200).send(group.messages);
+
+        } catch (error) {
+            console.error(error);
+            response.status(500).send({ message: error.message});
+        }
+    }
+
+    private async saveMessageToGroup(response: express.Response, request: express.Request) {
+        try {
+            let message : Message = JSON.parse(request.body.message)
+            const groupId = Number(request.params.groupId);
+            const group : ChatGroup = await AppDataSource.manager.findOne(ChatGroup, {
+                relations: ['messages'],
+                where: {id: groupId}
+            }); 
+
+            if(!group) {
+                throw new Error("Group not found!");
+            }
+            message = await AppDataSource.manager.save(Message, message);
+            group.messages.push(message);
+            await AppDataSource.manager.save(ChatGroup, group);
+
+            response.status(200).send('Message send!');
+        } catch (error) {
+            console.error(error);
+            response.status(500).send({ message: error.message});
+        }
     }
 }
