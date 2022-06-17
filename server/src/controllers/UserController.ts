3f22eb918d01b88559c001138d299fa64b01c2e0
nikolaTerziiski <nikolaterziiski@abv.bg>
2022-06-17 15:15:59 +0300

    register auth done

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -3,7 +3,14 @@ import { AppDataSource } from '../databates-connector';
 import { User } from '../entities/User.entity';
 import bcrypt from 'bcrypt';
 import { ChatGroup } from '../entities/ChatGroup.entity';
-import { chdir } from 'process';
+import { chdir, emit } from 'process';
+
+
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
 
 export class UserController {
     router: express.Router;
@@ -58,19 +65,51 @@ export class UserController {
 
     private async register(request: express.Request, response: express.Response) {
         try {
+            const username = request.body.username;
+            const password = request.body.password;
+            const email = request.body.email;
+
+            console.log(password);
+            if (!username || username.length < 3 || username.length > 100) {
+                throw "Make sure your username is more than 3 characters and less than 100";
+            }  
+            if (!password || password.length < 6 || password.length > 50) {
+                throw "Make sure your password is more than 6 characters and less than 50";
+            }
+            if(!email || !validateEmail(request.body.email)) {
+                throw "Invalid email. Please make sure is in correct format!";
+            }
+
+            if(AppDataSource.manager.findOne(User, {
+                where: {username: request.body.username}
+            })) {
+                throw "There is already a user with such username";
+            }
+
+            if(AppDataSource.manager.findOne(User, {
+                where: {email: email}
+            })) {
+                throw "There is already a user with such email";
+            }
+
             const hashedPassword = await bcrypt.hash(request.body.password, 10);
             const user : User = new User();
+
             user.username = request.body.username;
             user.password = hashedPassword;
+
             user.email = request.body.email;
             user.chatgroups = [];
 
             await AppDataSource.manager.save(User, user);
             delete(user.password);
             response.status(200).send(user);
-        } catch(error) {
-            console.error(error);
-            response.status(500).send({ message: error.message});
+        } catch(authError) {
+            response.status(400).send({
+                messageErr: authError
+            });
+            // console.error(error);
+            // response.status(500).send({ message: error.message});
         }
     }
 
